26adc8146d57065769e44d3d908a6e22f2b59129
Isaac-Gss <interdart93@gmail.com>
2025-05-13 02:06:44 -0300

    Add files via upload

---
+++ Estrutura Sequencial/Trabalho/src/main/java/com/mycompany/mavenproject3/Questão4.java
@@ -0,0 +1,24 @@
+// Questão 4: Estrutura de sequencia
+
+package com.mycompany.mavenproject3;
+
+import java.util.Scanner;
+
+public class Questão4 {
+
+
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite a base: ");
+            double base = scanner.nextDouble();
+            
+            System.out.print("Digite o expoente: ");
+            double expoente = scanner.nextDouble();
+            
+            double resultado = Math.pow(base, expoente);
+            
+            System.out.println(base + " elevado a " + expoente + " = " + resultado);
+        }
+    }
+}
